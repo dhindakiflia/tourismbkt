a4f4baafc6e8c9327b380db51dd00bebe8b70cbd
dhindakiflia <63203788+dhindakiflia@users.noreply.github.com>
2020-11-28 11:44:19 +0700

    Update verifikasi.php

--- tourism_bkt/admin/pages/verifikasi.php
+++ tourism_bkt/admin/pages/verifikasi.php
@@ -4,5 +4,5 @@ include ('../../../connect.php');
 $edit = pg_query("update admin set role='C' where username='$_GET[user]'");
 
 if($edit){
-	header('location:http://dhindakiflia-tourism.herokuapp.com/index.php');
+	header('location:https://dhindakiflia-tourism.herokuapp.com/index.php');
 }
